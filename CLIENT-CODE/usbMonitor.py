f313f8ead3ead78c90fbe4412974d59693c3619e
Raunak Ritesh <raunakritesh.india@gmail.com>
2019-03-21 00:24:13 +0530

    Partial Timeout Added

--- CLIENT-CODE/usbMonitor.py
+++ CLIENT-CODE/usbMonitor.py
@@ -1,4 +1,6 @@
 import pyudev
+import time
+from functools import partial
 
 def getNumberOfConnections(context):
 	totalConnected = 0
@@ -8,14 +10,25 @@ def getNumberOfConnections(context):
 	return totalConnected
 
 def synchronousMonitoring(context):
+	# Creates a Monitor which keeps on running
 	monitor = pyudev.Monitor.from_netlink(context)
 	monitor.filter_by('block')
-	for action, device in monitor:
-		if 'ID_FS_TYPE' in device:
-			print('{0} partition {1}'\
-					.format(action, device.get('ID_FS_LABEL')))
+
+	# action can be add, remove for the corresponding device
+	# for action, device in monitor:
+	# 	if 'ID_FS_TYPE' in device:
+	# 		print('{0} partition {1}'\
+	# 				.format(action, device.get('ID_FS_LABEL')))
+
+	# Runs for the next 2 hours once an event is logged
+	for device in iter(partial(monitor.poll, 7200), None):
+		print('{0.action} on {1}' .format(device, device.get('ID_FS_LABEL')))
 
 if __name__ == "__main__":
 	context = pyudev.Context()
 	print(getNumberOfConnections(context))
+
+	t0 = time.time()
+
+
 	synchronousMonitoring(context)
