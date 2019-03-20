8fa2e1fa243e0c5d1a602d26f445ed2b57a65daf
Raunak Ritesh <raunakritesh.india@gmail.com>
2019-03-20 23:56:44 +0530

    Synchronous Monitor Added for USB Tool'

--- CLIENT-CODE/usbMonitor.py
+++ CLIENT-CODE/usbMonitor.py
@@ -7,6 +7,15 @@ def getNumberOfConnections(context):
 		totalConnected += 1
 	return totalConnected
 
+def synchronousMonitoring(context):
+	monitor = pyudev.Monitor.from_netlink(context)
+	monitor.filter_by('block')
+	for action, device in monitor:
+		if 'ID_FS_TYPE' in device:
+			print('{0} partition {1}'\
+					.format(action, device.get('ID_FS_LABEL')))
+
 if __name__ == "__main__":
 	context = pyudev.Context()
 	print(getNumberOfConnections(context))
+	synchronousMonitoring(context)
