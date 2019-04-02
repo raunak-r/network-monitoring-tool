d0a452d96d4c95687ab826056422542b7c9b7e65
Raunak Ritesh <raunakritesh.india@gmail.com>
2019-04-02 13:06:59 +0530

    POST Method Code Framework Added

--- LabMonitor/views.py
+++ LabMonitor/views.py
@@ -2,8 +2,22 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generics import View
-from django.http import HttpRepsponse, JsonResponse
+from django.views.generic import View
+from django.http import HttpResponse, JsonResponse
+
+import json, os, io
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Logs(View):
 	def post(self, request):
+		info = {
+			'hostname' : request.POST.get('hostname', ''),
+			'ip' : request.POST.get('ip', ''),
+			'message' : request.POST.get('message', '')
+		}
+
+		with open(BASE_DIR + '/../file.txt', 'w') as outfile:
+			outfile.write(json.dumps(info))
+
+		return HttpResponse('Info Saved Successfully', status = 200)
